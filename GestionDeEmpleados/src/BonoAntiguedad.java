60c5dcf7885fe9728423aa8a05bc9c89a15ffa61
Tobias Traverso <tobitraverso@gmail.com>
2024-10-21 21:09:43 -0300

    Agrego calculo de bono por antiguedad

--- GestionDeEmpleados/src/BonoAntiguedad.java
+++ GestionDeEmpleados/src/BonoAntiguedad.java
@@ -1,22 +1,23 @@
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
-public class BonoAntiguedad extends Bono{
+public class BonoAntiguedad extends Bono {
     @Override
     public int calcularBono(Empleado empleado) {
         long diasEntreFechaIngresoYHoy = ChronoUnit.DAYS.between(empleado.getFechaDeIngreso(), LocalDate.now());
 
-        if(diasEntreFechaIngresoYHoy >= 180) { //significa que el empleado ya tiene al menos 6 meses de antiguedad
+        if (diasEntreFechaIngresoYHoy >= 180) { // Al menos 6 meses de antigüedad
             int sueldo = empleado.getCargo().getSueldoBasico();
-            int horasQueTrabaja = empleado.getContrato().getCantidadDeHoras();
+            int horasContrato = empleado.getContrato().getCantidadDeHoras();
 
-            // falta definir el algoritmo para el calculo del bono de antiguedad teniendo en cuenta lo de arriba
-            // return
-        }
-        else { //no tiene 6 meses de antiguedad, entonces no cobra el bono
-            return 0;
-        }
+            // Cálculo del bono: proporcional en relacion a las horas según contrato +  proporcional en relación al tope de años en cuenta respecto a años de trabajo (10 años)
+            double bono = sueldo * (horasContrato / 8.0) + sueldo * ((diasEntreFechaIngresoYHoy / 365.0) / 10.0);
+            // Es decir, se agrega 0,5 de un sueldo basico a parttimers y 1 a fulltimets + un bono de 1 sueldo a quien lleve 10 años o más en la empresa, o el proporcional a un sueldo segun cantidad de años.
 
-        return 0;
+            // Retornar el bono como entero
+            return (int) bono;
+        } else {
+            return 0; // No tiene 6 meses de antigüedad, no recibe bono
+        }
     }
 }
