538376a0d5f21e0c0372a103d8ed540edf71075a
kate <katerinacejas3@gmail.com>
2024-11-07 19:50:08 -0300

    arranco con la clase empresa

--- GestionDeEmpleados/src/Empresa.java
+++ GestionDeEmpleados/src/Empresa.java
@@ -11,6 +11,19 @@ public class Empresa {
         this.empleados.add(unEmpleado);
     }
 
+    public void despedirEmpleado(Empleado unEmpleado) {
+        this.empleados.remove(unEmpleado);
+    }
+
+    public Set<Empleado> getEmpleados() {
+        return this.empleados;
+    }
+
+    public long porcentajeEmpleadosConMasDe10AniosAntiguedad() {
+        return this.empleados.stream().filter(empleado -> empleado.getAntiguedadEnAnios() >= 10).count()
+                * 100 / this.empleados.size();
+    }
+
     /*
         hacer metodos para estadisticas (idea del profe):
         1) cantidad de empleados con buen rendimiento (rendimiendo = 4 o 5)
@@ -19,10 +32,11 @@ public class Empresa {
         4) cantidad de empleados que no se tomaron licencias en el mes actual
         5) cantidad de empleados con mas de 1 titulo de estudio
         6) total de costo de empleados para la empresa (es la sumatoria de los sueldos finales de cada empleado)
-        7) porcentaje de empleados con mas de 10 años de antiguedad
 
         alguna otra idea que se les ocurra es bienvenida. esto es para jugar con las colecciones que pidió
 
 
      */
+
+
 }
